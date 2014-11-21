64af95329385608e8d1ed4b1130c4a5643537289
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2014-11-21 16:03:07 -0500

    initial commit with mod .gitignore

---
+++ README.md
@@ -0,0 +1,80 @@
+# Web Eyes Got to Real Time (WEG2RT) #
+
+* Prototype for initial version of an Collaborative Augmented Information Sharing System
+* Version: 0.1
+* Original based on webrtc.io  (https://github.com/webRTC/webrtc.io-demo)
+
+## Technologies
+
+* node.js
+* express - node  application framework (http://expressjs.com/)
+* Handlebars - semantic templating (http://handlebarsjs.com/)
+* webrtc.io - javascript (RTC) -  (https://github.com/webRTC/webRTC.io)
+* WebGL -  web 3D graphics (http://get.webgl.org/, http://www.chromeexperiments.com/webgl/)
+* EasyRTC
+* buildAR
+* Jasmine - TDD framework
+
+## Repository:
+
+    https://bitbucket.org/sw_hansen/weg2rt
+
+## Deploy: 
+
+[Heroku Dev Center](https://devcenter.heroku.com/articles/getting-started-with-nodejs#introduction)
+
+## Testing
+
+## Protocol, Best Practices, Style
+
+Use the following guides for getting things done, programming well, and
+programming in style.
+
+* [Protocol](http://github.com/thoughtbot/guides/blob/master/protocol)
+* [Best Practices](http://github.com/thoughtbot/guides/blob/master/best-practices)
+* [Style](http://github.com/thoughtbot/guides/blob/master/style)
+* [Javascript Style Google](https://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml)
+
+### Contribution guidelines ###
+
+* Writing tests
+* Code review
+* Other guidelines
+
+### Who do I talk to? ###
+
+Steve Hansen - sw_hansen@obliquevision.org
+
+
+##Instructions on local setup for demo (node.js must be installed)
+
+Run in your terminal
+
+```bash
+git clone https://sw_hansen@bitbucket.org/sw_hansen/weg2rt.git
+```
+
+```bash
+cd weg2rt
+```
+
+```bash
+npm install
+```
+
+```bash
+node server.js
+```
+
+visit [localhost:8080/login](http://localhost:8080/login)
+
+enter the password (email me)
+
+click allow to see your camera
+
+in a new tab go to [localhost:8080](http://localhost:8080)
+
+click allow to see your camera and the connection will be made between your two open windows.
+
+
+
---
+++ server.js
@@ -0,0 +1,121 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var io = require("socket.io");
+var easyrtc = require("easyrtc");
+
+app.use("/js", express.static(__dirname + '/easyrtc/js'));
+app.use("/images", express.static(__dirname + '/easyrtc/images'));
+app.use("/css", express.static(__dirname + '/easyrtc/css'));
+
+//For the test homepage
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use("/css", express.static(__dirname + '/public/css'));
+app.use("/img", express.static(__dirname + '/public/img'));
+
+// Needed to parse form data(changed for express 4.x)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//global variables
+var loggedIn = false;
+var password = 'weg2rt';
+
+//Test home page
+app.get('/', function (req, res) {
+  if (loggedIn === true) {
+    res.render('index', {
+      title: 'available',
+      message: "WEG2RT is ready"
+    });
+    console.log("homepage -logged in");
+  } else {
+    res.render('index', {
+      title: 'not',
+      message: "WEG2RT does not have anyone logged in"
+    });
+    console.log("homepage -not logged in");
+  }
+});
+
+//Serve a static login page if not logged in already
+app.get('/login', function (req, res) {
+  console.log('Login attempt');
+  if (loggedIn === true) {
+    res.sendfile(__dirname + '/views/multiparty.html');
+  } else {
+    res.sendfile(__dirname + '/views/login.html');
+  }
+});
+
+//Respond to POST from login form
+app.post('/login', function (req, res) {
+  if (loggedIn === true) {
+    res.send("Already logged in.");
+  } else {
+    console.log("Posted data:" + JSON.stringify(req.body));
+    if (req.body.pw === password) {
+      loggedIn = true;
+      res.send("logged in");
+      console.log("Logged in");
+
+      // Start EasyRTC server
+      var easyrtcServer = easyrtc.listen(app, socketServer, {
+        'apiEnable': 'true'
+      });
+
+    } else {
+      res.send("Incorrect password.");
+    }
+  }
+});
+
+//Serve a static logout page
+app.get('/logout', function (req, res) {
+  if (loggedIn === true) {
+    res.sendfile(__dirname + '/public/logout.html');
+    console.log("logout page");
+
+  } else {
+    res.send("Not logged in.");
+  }
+});
+
+//Check the password to prevent unauthoried logouts
+app.post('/logout', function (req, res) {
+  console.log("Posted data:" + JSON.stringify(req.body));
+  if (req.body.pw === password) {
+    if (loggedIn === true) {
+      loggedIn = false;
+      res.send("Logged out");
+      console.log("logged out");
+
+      //Consider killing all active sessions here
+      easyrtc.setOption('apiEnable', 'false');
+    } else {
+      res.send("You were already logged out");
+      console.log("Attempt to logout when not logged in");
+    }
+  } else {
+    console.log("Bad password attempt");
+    res.send("Incorrect password");
+  }
+});
+
+//Initiate a video call
+app.get('/video', function (req, res) {
+  if (loggedIn === true) {
+    res.sendfile(__dirname + '/views/multiparty.html');
+  } else {
+    res.send("Please try later.");
+  }
+});
+var port = process.env.PORT || 8080;
+// var webServer = app.listen(process.env.port || 8080); //for running localhost port
+var webServer = app.listen(port);
+console.log('Listening on port ' + 8080);
+
+// Start Socket.io so it attaches itself to Express server
+var socketServer = io.listen(webServer);
