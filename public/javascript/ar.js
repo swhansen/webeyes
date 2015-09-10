80f15adb2f1a5eb2e957e086ec1be71cbac04b82
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2015-09-10 13:39:55 -0400

    wip, cleanup

--- public/javascript/ar.js
+++ public/javascript/ar.js
@@ -3,23 +3,25 @@ var arPane     = document.getElementById( 'arcanvaspane' );
 
 function orientationAr() {
 
-  document.getElementById( 'cube' ).style.visibility = 'visible';
-  document.getElementById( 'cube' ).style.zIndex = '99';
+  document.getElementById('cube').style.visibility = 'visible';
+  document.getElementById('cube').style.zIndex = '99';
 
-  if ( !window.DeviceOrientationEvent ) {
-            console.log( "no device orientstion" );
+  if (!window.DeviceOrientationEvent) {
+            console.log("no device orientstion");
      } else {
-        window.addEventListener( 'deviceorientation', function( event ) {
-           document.getElementById( 'cube' ).style.webkitTransform =
-           document.getElementById( 'cube' ).style.transform =
+        window.addEventListener('deviceorientation', function(event) {
+           document.getElementById('cube').style.webkitTransform =
+           document.getElementById('cube').style.transform =
                    'rotateX(' + event.beta + 'deg) ' +
                    'rotateY(' + event.gamma + 'deg) ' +
                    'rotateZ(' + event.alpha + 'deg)';
+        });
+     }
        }
 
 function geometryAr() {
 
-  document.getElementById( "arcanvaspane" ).className = "canvascenter";
+  document.getElementById("arcanvaspane").className = "canvascenter";
 
   arCanvas.style.width = '100%';
   arCanvas.style.height = '100%';
@@ -43,7 +45,7 @@ camera = new THREE.PerspectiveCamera( 50, CANVAS_WIDTH / CANVAS_HEIGHT, 1, 1000 
 
 // set the renderer to the AR canvas
 
-renderer = new THREE.WebGLRenderer( { canvas: arCanvas, alpha: true } );
+renderer = new THREE.WebGLRenderer({ canvas: arCanvas, alpha: true});
 renderer.setSize( box0Width, box0Width );
 
 renderer.setClearColor( 0x000000, 0 );
@@ -57,7 +59,7 @@ var radius = .3,
   var sphereMaterial = new THREE.MeshLambertMaterial(
     {
       color: 'blue'
-    } );
+    });
 
 // create a new mesh with
 // sphere geometry - we will cover
@@ -65,20 +67,20 @@ var radius = .3,
 var sphere = new THREE.Mesh( new THREE.SphereGeometry(
     radius,
     segments,
-    rings ),
-  sphereMaterial );
+    rings),
+  sphereMaterial);
 
-sphere.position.set( -1., 1.0, .2 );
-
+sphere.position.set(-1., 1.0, .2);
 //sphere.position.x = -2.0;
 sphere.position.y = 1.0;
-
 //sphere.position.z = .2;
-scene.add( sphere );
+scene.add(sphere);
+
 
 pivotPoint = new THREE.Object3D();
 pivotPoint.rotation.x = 0.4;
-sphere.add( pivotPoint );
+sphere.add(pivotPoint);
+
 
 // add the sphere to the scene
 //scene.add(sphere);
@@ -89,7 +91,7 @@ var geometry = new THREE.BoxGeometry( .3, .5, .7 );
 var material = new THREE.MeshLambertMaterial( { color: 'red' } );
 var cube = new THREE.Mesh( geometry, material );
 cube.castShadow = true;
-cube.position.set( 2.5, 1.0, 1.0 );
+cube.position.set(2.5, 1.0, 1.0);
 pivotPoint.add( cube );
 
 //  light
@@ -97,25 +99,25 @@ pivotPoint.add( cube );
 camera.position.x = 4;
   camera.position.y = 4;
   camera.position.z = 4;
-  camera.lookAt( scene.position );
+  camera.lookAt(scene.position);
 
  var light = new THREE.SpotLight();
-    light.position.set( 40, 4, 40 );
+    light.position.set(40, 4, 40);
     light.castShadow = true;
     light.shadowMapEnabled = true;
     light.shadowCameraNear = 20;
     light.shadowCameraFar = 100;
     ;
 
-    scene.add( light );
+    scene.add(light);
+
 
 function render() {
-
   // control the rotation rate
 
   var fps = 20;
 
-  setTimeout( function() {
+  setTimeout(function() {
 
   requestAnimationFrame( render );
 
@@ -126,7 +128,7 @@ function render() {
   cube.rotation.y += 0.1;
 
   renderer.render( scene, camera );
-}, 1000 / fps );
+}, 1000 / fps);
 
 };
 
