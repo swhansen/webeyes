5ae58ffbd74782c15a7d454286e28652c9ce53ba
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2015-09-10 13:32:49 -0400

    AR objects motion, cameras, lighting

--- public/javascript/ar.js
+++ public/javascript/ar.js
@@ -2,56 +2,24 @@ var arCanvas     = document.getElementById( 'ar-canvas' );
 var arPane     = document.getElementById( 'arcanvaspane' );
 
 function orientationAr() {
-  document.getElementById('cube').style.visibility = 'visible';
 
-  if (!window.DeviceOrientationEvent) {
-            console.log("no device orientstion");
-         } else {
+  document.getElementById( 'cube' ).style.visibility = 'visible';
+  document.getElementById( 'cube' ).style.zIndex = '99';
 
-
-            window.addEventListener('deviceorientation', function(event) {
-               document.getElementById('cube').style.webkitTransform =
-               document.getElementById('cube').style.transform =
-                       'rotateX(' + event.beta + 'deg) ' +
-                       'rotateY(' + event.gamma + 'deg) ' +
-                       'rotateZ(' + event.alpha + 'deg)';
-
-        //       document.getElementById('beta').innerHTML = Math.round(event.beta);
-        //       document.getElementById('gamma').innerHTML = Math.round(event.gamma);
-        //       document.getElementById('alpha').innerHTML = Math.round(event.alpha);
-        //       document.getElementById('is-absolute').innerHTML = event.absolute ? "true" : "false";
-            });
-         }
-
-         //if (!window.DeviceMotionEvent) {
-         //   document.getElementById('dm-unsupported').classList.remove('hidden');
-         //} else {
-         //  // document.getElementById('dm-info').classList.remove('hidden');
-//
-         //   window.addEventListener('devicemotion', function(event) {
-         //      document.getElementById('acceleration-x').innerHTML = Math.round(event.acceleration.x);
-         //      document.getElementById('acceleration-y').innerHTML = Math.round(event.acceleration.y);
-         //      document.getElementById('acceleration-z').innerHTML = Math.round(event.acceleration.z);
-//
-         //      document.getElementById('acceleration-including-gravity-x').innerHTML =
-         //              Math.round(event.accelerationIncludingGravity.x);
-         //      document.getElementById('acceleration-including-gravity-y').innerHTML =
-         //              Math.round(event.accelerationIncludingGravity.y);
-         //      document.getElementById('acceleration-including-gravity-z').innerHTML =
-         //              Math.round(event.accelerationIncludingGravity.z);
-//
-         //      document.getElementById('rotation-rate-beta').innerHTML = Math.round(event.rotationRate.beta);
-         //      document.getElementById('rotation-rate-gamma').innerHTML = Math.round(event.rotationRate.gamma);
-         //      document.getElementById('rotation-rate-alpha').innerHTML = Math.round(event.rotationRate.alpha);
-//
-         //      document.getElementById('interval').innerHTML = event.interval;
-         //   });
-         //}
+  if ( !window.DeviceOrientationEvent ) {
+            console.log( "no device orientstion" );
+     } else {
+        window.addEventListener( 'deviceorientation', function( event ) {
+           document.getElementById( 'cube' ).style.webkitTransform =
+           document.getElementById( 'cube' ).style.transform =
+                   'rotateX(' + event.beta + 'deg) ' +
+                   'rotateY(' + event.gamma + 'deg) ' +
+                   'rotateZ(' + event.alpha + 'deg)';
        }
 
 function geometryAr() {
 
-  document.getElementById("arcanvaspane").className = "canvascenter";
+  document.getElementById( "arcanvaspane" ).className = "canvascenter";
 
   arCanvas.style.width = '100%';
   arCanvas.style.height = '100%';
@@ -64,68 +32,104 @@ function geometryAr() {
   document.getElementById( 'arcanvaspane' ).style.visibility = 'visible';
   document.getElementById( 'arcanvaspane' ).offsetHeight = box0Height;
   document.getElementById( 'arcanvaspane' ).offsetWidth = box0Width;
- // document.getElementById( 'arcanvaspane' ).zIndex = 500;
- // document.getElementById( 'ar-canvas' ).zIndex = 500;
 
   var container, camera, scene, renderer, mesh,
-
-    mouse = { x: 0, y: 0 },
-    objects = [],
-
-    count = 0,
-
-    CANVAS_WIDTH = 200,
-    CANVAS_HEIGHT = 200;
-
-// set the renderer to the AR canvas
-
-renderer = new THREE.WebGLRenderer({ canvas: arCanvas, alpha: true});
-renderer.setSize( box0Width, box0Width );
-
-renderer.setClearColor( 0x000000, 0 );
-
-renderer.shadowMapEnabled = true;
-
-//arPane.appendChild( renderer.domElement );
+    CANVAS_WIDTH = 300,
+    CANVAS_HEIGHT = 300;
 
 scene = new THREE.Scene();
 
 camera = new THREE.PerspectiveCamera( 50, CANVAS_WIDTH / CANVAS_HEIGHT, 1, 1000 );
-camera.position.y = 200;
-camera.position.z = 500;
-camera.lookAt( scene.position );
 
-mesh = new THREE.Mesh(
-    new THREE.BoxGeometry( 50, 50, 50, 1, 1, 1 ),
-    new THREE.MeshBasicMaterial( {color: 0xfffff})
- );
+// set the renderer to the AR canvas
 
-mesh.castShadow = true;
+renderer = new THREE.WebGLRenderer( { canvas: arCanvas, alpha: true } );
+renderer.setSize( box0Width, box0Width );
 
-var spotLight = new THREE.SpotLight(0xffffff);
-spotLight.position.set(10, 20, 20);
-spotLight.castShadow = true;
-scene.add(spotLight);
+renderer.setClearColor( 0x000000, 0 );
 
-var hex  = 0xff0000;
-var bbox = new THREE.BoundingBoxHelper( mesh, hex );
-bbox.update();
-scene.add( bbox );
+//sphere
 
-scene.add( mesh );
-objects.push( mesh );
+var radius = .3,
+    segments = 16,
+    rings = 16;
 
-// find intersections
-var vector = new THREE.Vector3();
-var raycaster = new THREE.Raycaster();
+  var sphereMaterial = new THREE.MeshLambertMaterial(
+    {
+      color: 'blue'
+    } );
+
+// create a new mesh with
+// sphere geometry - we will cover
+// the sphereMaterial next!
+var sphere = new THREE.Mesh( new THREE.SphereGeometry(
+    radius,
+    segments,
+    rings ),
+  sphereMaterial );
+
+sphere.position.set( -1., 1.0, .2 );
+
+//sphere.position.x = -2.0;
+sphere.position.y = 1.0;
+
+//sphere.position.z = .2;
+scene.add( sphere );
+
+pivotPoint = new THREE.Object3D();
+pivotPoint.rotation.x = 0.4;
+sphere.add( pivotPoint );
+
+// add the sphere to the scene
+//scene.add(sphere);
+
+//cube
+
+var geometry = new THREE.BoxGeometry( .3, .5, .7 );
+var material = new THREE.MeshLambertMaterial( { color: 'red' } );
+var cube = new THREE.Mesh( geometry, material );
+cube.castShadow = true;
+cube.position.set( 2.5, 1.0, 1.0 );
+pivotPoint.add( cube );
+
+//  light
+
+camera.position.x = 4;
+  camera.position.y = 4;
+  camera.position.z = 4;
+  camera.lookAt( scene.position );
+
+ var light = new THREE.SpotLight();
+    light.position.set( 40, 4, 40 );
+    light.castShadow = true;
+    light.shadowMapEnabled = true;
+    light.shadowCameraNear = 20;
+    light.shadowCameraFar = 100;
+    ;
+
+    scene.add( light );
 
 function render() {
-    mesh.rotation.y += 0.01;
-    renderer.render( scene, camera );
-}
 
-(function animate() {
-    requestAnimationFrame( animate );
-    render();
-})();
+  // control the rotation rate
+
+  var fps = 20;
+
+  setTimeout( function() {
+
+  requestAnimationFrame( render );
+
+  pivotPoint.rotation.x += .1;
+  pivotPoint.rotation.y += .1;
+
+  cube.rotation.x += 0.1;
+  cube.rotation.y += 0.1;
+
+  renderer.render( scene, camera );
+}, 1000 / fps );
+
+};
+
+render();
+
 }
