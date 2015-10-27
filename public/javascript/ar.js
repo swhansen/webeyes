eb2240e20ce8dced04a8a94ddda227d54462bfee
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2015-10-27 17:41:51 -0400

    added orientation objects

--- public/javascript/ar.js
+++ public/javascript/ar.js
@@ -315,7 +315,8 @@ $( '#ar-canvas' ).longpress( function( event ) {
 
 // add the object locally and tell everyone else
 
-      addArObject( pos.x, pos.y, pos.z );
+     // addArObject( pos.x, pos.y, pos.z );
+
       emitArObject( arShareData );
 
   return;
