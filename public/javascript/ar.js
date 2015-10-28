e8cee176ab9a7ba4ceb417bfd6501140ecefdf8d
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2015-10-28 13:32:26 -0400

    model modification

--- public/javascript/ar.js
+++ public/javascript/ar.js
@@ -149,11 +149,11 @@ function setUpArLayer( participantState ) {
   cube2.rotateZ = 10.00;
 
   var loader = new THREE.JSONLoader();
-  loader.load( '../armodels/sheep.json', function( model ) {
+  loader.load( '../armodels/sheep2.json', function( model ) {
     var material = new THREE.MeshPhongMaterial( { color: 0xFF69B4 } );
     sheep = new THREE.Mesh( model, material );
 
-    sheep.scale.set( 1, 1, 2 );
+    sheep.scale.set( 0.1, 0.1, 0.1 );
     sheep.position.set( -2.0, -0.4, 0.0 );
     sheep.rotation.x = Math.PI / 2;
     sheep.rotation.y = ( Math.PI / 2 ) * 0.5;
