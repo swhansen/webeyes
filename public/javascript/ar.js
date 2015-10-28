d2ca7512f8a684282c28cb044112c2613d0f7897
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2015-10-28 13:20:20 -0400

    model modification

--- public/javascript/ar.js
+++ public/javascript/ar.js
@@ -153,7 +153,7 @@ function setUpArLayer( participantState ) {
     var material = new THREE.MeshPhongMaterial( { color: 0xFF69B4 } );
     sheep = new THREE.Mesh( model, material );
 
-    sheep.scale.set( 0.1, 0.1, 0.1 );
+    sheep.scale.set( 0.5, 0.5, 0.5 );
     sheep.position.set( -2.0, -0.4, 0.0 );
     sheep.rotation.x = Math.PI / 2;
     sheep.rotation.y = ( Math.PI / 2 ) * 0.5;
