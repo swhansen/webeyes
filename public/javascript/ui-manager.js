63fed4720bba31e95fbb389abce2e59f6ceb80af
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2015-11-02 09:17:31 -0500

    ar workflow redesign

--- public/javascript/ui-manager.js
+++ public/javascript/ui-manager.js
@@ -149,7 +149,7 @@ function augmeUI() {
 
   loadAr( userContext.participantState );
 
-  document.getElementById( 'stick-ar' ).style.visibility = 'visible';
+  document.getElementById( 'sticky-ar' ).style.visibility = 'visible';
 
   setDomMouseEvent( 'canvas0', 'none' );
   setDomMouseEvent( 'arcanvaspane', 'auto' );
@@ -177,7 +177,7 @@ function shareAr() {
 
     emitArOrientationData();
 
-    document.getElementById( 'stick-ar' ).style.visibility = 'visible';
+    document.getElementById( 'sticky-ar' ).style.visibility = 'visible';
 }
 
 //
