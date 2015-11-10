77fd1df8b373457e1a57f6e48384f4479ce82243
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2015-11-10 15:43:15 -0500

    video device enumeration

--- public/javascript/static-multiparty.js
+++ public/javascript/static-multiparty.js
@@ -877,13 +877,16 @@ easyrtc.setVideoSrc( device.id );
      }]
    }
  };
+
  function successCallback(stream) {
  window.stream = stream; // make stream available to console
  videoElement.src = window.URL.createObjectURL(stream);
  videoElement.play();
+}
 
  function errorCallback(error) {
  console.log('navigator.getUserMedia error: ', error);
+}
 
  navigator.getUserMedia(constraints, successCallback, errorCallback);
 
