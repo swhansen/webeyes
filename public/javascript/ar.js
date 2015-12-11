bd5ad255e53ca0d1df5220745171c8d92acfea5e
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2015-12-11 11:44:50 -0500

    flying Pig

--- public/javascript/ar.js
+++ public/javascript/ar.js
@@ -8,6 +8,7 @@ var sheep;
 var sheep2;
 var pigModel;
 var pivotPoint;
+var lamp;
 var torus1;
 var counter = 0;
 
@@ -219,7 +220,7 @@ loader.load( '../armodels/lamp.json', function( model ) {
     var material = new THREE.MeshPhongMaterial( { color: 0xFF69B4 } );
 
     lamp = new THREE.Mesh( model, material );
-    lamp.scale.set( 0.1, 0.1, 0.1 );
+    lamp.scale.set( 500, 500, 500 );
     lamp.position.set( -4.0, 4.4, -3.0 );
     //lamp.rotation.x = Math.PI / 2;
     //lamp.rotation.y = ( Math.PI / 2 ) * 0.5;
