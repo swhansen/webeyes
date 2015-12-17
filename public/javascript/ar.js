6536d61a091f75fbcd4aa89c2d94c1c1d4a98a9d
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2015-12-17 09:01:19 -0500

    AR select swordGuy

--- public/javascript/ar.js
+++ public/javascript/ar.js
@@ -109,7 +109,6 @@ function loadAr( participantState ) {
       torus1.name = 'torus1';
       scene.add( torus1 );
       arSelectObjectArray.push( torus1 );
-
     }
   }
 
@@ -262,26 +261,29 @@ function setUpArLayer( participantState ) {
           m.color.setHSL( 0.6, 0, 0.6 );
         }
 
-        mesh = new THREE.SkinnedMesh( geometry, new THREE.MeshFaceMaterial( materials ) );
-        mesh.position.set( x, y - bb.min.y * s, z );
-        mesh.scale.set( s, s, s );
-        mesh.rotation.y =  -Math.PI;
-        scene.add( mesh );
+        swordGuyMesh = new THREE.SkinnedMesh( geometry, new THREE.MeshFaceMaterial( materials ) );
+        swordGuyMesh.position.set( x, y - bb.min.y * s, z );
+        swordGuyMesh.scale.set( s, s, s );
+        swordGuyMesh.rotation.y =  -Math.PI;
+        swordGuyMesh.name = 'swordGuy';
+        scene.add( swordGuyMesh );
 
-     //   mesh.castShadow = true;
-     //   mesh.receiveShadow = true;
+     //   swordGuyMesh.castShadow = true;
+     //   swordGuyMesh.receiveShadow = true;
 
-        helper = new THREE.SkeletonHelper( mesh );
+        helper = new THREE.SkeletonHelper( swordGuyMesh );
         helper.material.linewidth = 3;
         helper.visible = false;
         scene.add( helper );
 
-        var clipMorpher = THREE.AnimationClip.CreateFromMorphTargetSequence( 'facialExpressions', mesh.geometry.morphTargets, 3 );
+        var clipMorpher = THREE.AnimationClip.CreateFromMorphTargetSequence( 'facialExpressions', swordGuyMesh.geometry.morphTargets, 3 );
         var clipBones = geometry.animations[0];
 
-        mixer = new THREE.AnimationMixer( mesh );
+        mixer = new THREE.AnimationMixer( swordGuyMesh );
         mixer.addAction( new THREE.AnimationAction( clipMorpher ) );
         mixer.addAction( new THREE.AnimationAction( clipBones ) );
+
+        arSelectObjectArray.push( swordGuyMesh );
       }
 
   scene.add( cube2 );
