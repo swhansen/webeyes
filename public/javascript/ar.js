a215978089ddd27f4fd5cf8565406a3b14cb627f
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2015-12-22 12:51:07 -0500

    dynamic selectable objects

--- public/javascript/ar.js
+++ public/javascript/ar.js
@@ -563,7 +563,7 @@ function addNewArObjectToWorld( d ) {
       console.log( 'intersected object:', intersects[0], intersects[0].object.name );
       var selectedObject = scene.getObjectByName( intersects[0].object.name );
       console.log( 'SelectedObject:', selectedObject );
-      //console.log( 'SelectedObject-userData:', selectedObject );
+      console.log( 'SelectedObject-userData:', selectedObject.userData );
 
       //intersects[0].object.userData = { isAnimated: true };
      // console.log( ' userData:', intersects[0].object.userData );
