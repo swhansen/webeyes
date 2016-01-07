7889741e174229cca6623a3d9f67fbe06eb31b3f
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-01-07 12:10:27 -0500

    initial Leap Motion dev

--- public/javascript/static-multiparty.js
+++ public/javascript/static-multiparty.js
@@ -1119,7 +1119,48 @@ setBrowserDetails();
 
 initDraw();
 initUtil();
-setUpLeapLayer();
+Leap.loop()
+    .use('boneHand', {
+      targetEl: document.body,
+      arm: true,
+      opacity: 0.5
+    } );
+
+  // Set up scene
+
+  var scene    = Leap.loopController.plugins.boneHand.scene;
+  var camera   = Leap.loopController.plugins.boneHand.camera;
+  var renderer = Leap.loopController.plugins.boneHand.renderer;
+
+  var plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(80,80),
+    new THREE.MeshPhongMaterial({wireframe: false})
+  );
+
+
+  plane.scale.set(2,2,2);
+  plane.position.set(0,200,-100);
+
+  camera.lookAt( plane.position );
+
+//  scene.add(plane);
+
+  var axisHelper = new THREE.AxisHelper( 100 );
+  scene.add( axisHelper );
+
+  var controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+
+  Leap.loopController.on('handFound', function(hand) {
+    document.querySelector('canvas').style.display = 'block';
+  }).on('handLost', function(hand){
+    if (Leap.loopController.frame(0).hands.length === 0){
+      document.querySelector('canvas').style.display = 'none';
+    }
+  });
+
+  document.getElementById('view-source').href = "view-source:" + window.location.href;
+  document.getElementById('view-source').target = "_blank";
 messageBar( 'User Session Initialized' );
 
 }
