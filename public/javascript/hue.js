e6009e5ad4eb66d83a418169df08e4db6cb6ba20
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-01-09 16:55:40 -0500

    initial Leap Motion dev

--- public/javascript/hue.js
+++ public/javascript/hue.js
@@ -1,16 +1,15 @@
 
-var hueURL = 'http://10.0.1.24/api/4cca312bfd9d1976814b78d491ecd8b/lights/1/state';
-
-var dataObject = { 'on':true, 'hue': 44560 };
-
-var dataObject = { 'on':false, 'hue': 44560 };
+//var hueURL = 'http://10.0.1.24/api/4cca312bfd9d1976814b78d491ecd8b/lights/1/state';
+//
+//var dataObject = { 'on':true, 'hue': 44560 };
+//
+//var dataObject = { 'on':false, 'hue': 44560 };
 
 var hueLightState = function( lightNo, state) {
 
-
-var dataObject =  {};
-dataObject.on = state;
-dataObject.lightNo = lightNo;
+    var dataObject =  {};
+    dataObject.on = state;
+    var hueURL = 'http://10.0.1.24/api/4cca312bfd9d1976814b78d491ecd8b/lights/' + lightNo + '/state';
 
     $.ajax( {
     type: 'PUT',
@@ -20,4 +19,4 @@ dataObject.lightNo = lightNo;
     success: function( data ) { },
     error: function( a, err ) { }
     } );
-}
+};
