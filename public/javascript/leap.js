f10c7ee793da4a23a0c238e8a193b4cbd24cb530
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-01-18 11:54:26 -0500

    initial leap integration

--- public/javascript/leap.js
+++ public/javascript/leap.js
@@ -18,27 +18,28 @@ Leap.loop()
   var camera   = Leap.loopController.plugins.boneHand.camera;
   var renderer = Leap.loopController.plugins.boneHand.renderer;
 
-  var plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(80,80),
-    new THREE.MeshPhongMaterial({wireframe: false})
-  );
-
-  plane.scale.set(2,2,2);
-  plane.position.set(0,200,-100);
+//  var plane = new THREE.Mesh(
+//    new THREE.PlaneGeometry(80,80),
+//    new THREE.MeshPhongMaterial({wireframe: false})
+//  );
+//
+//
+//  plane.scale.set(2,2,2);
+//  plane.position.set(0,200,-100);
 
   camera.lookAt( plane.position );
 
 //  scene.add(plane);
 
-//  var axisHelper = new THREE.AxisHelper( 100 );
-//  scene.add( axisHelper );
+  var axisHelper = new THREE.AxisHelper( 100 );
+  scene.add( axisHelper );
 
   var controls = new THREE.OrbitControls( camera, renderer.domElement );
 
   Leap.loopController.on( 'handFound' , function( hand ) {
     document.getElementById( 'canvas' ).style.display = 'block';
   } ).on( 'handLost', function( hand ) {
-    if ( Leap.loopController.frame( 0 ).hands.length === 0 ) {
+    if ( Leap.loopController.frame( 0 ).hands.length === 0 ){
       document.getElementById( 'canvas' ).style.display = 'none';
     }
   } );
