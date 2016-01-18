2c3f10a4cf1369cd200726c9ea3548d7749588d6
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-01-18 11:44:28 -0500

    initial leap integration

--- public/javascript/leap.js
+++ public/javascript/leap.js
@@ -1,9 +1,9 @@
 
 function initLeap() {
 
-  console.log( 'at initLeap' );
+  console.log( 'at setUpLeapLayer' );
 
-var leapFullScreen = document.getElementById( 'leapfull' );
+  var leapFullScreen = document.getElementById( 'leapfull' );
 
 Leap.loop()
     .use( 'boneHand', {
@@ -12,17 +12,20 @@ Leap.loop()
       opacity: 0.5
     } );
 
+  // Set up scene
+
   var scene    = Leap.loopController.plugins.boneHand.scene;
   var camera   = Leap.loopController.plugins.boneHand.camera;
   var renderer = Leap.loopController.plugins.boneHand.renderer;
 
-  var plane = new THREE.Mesh(
-    new THREE.PlaneGeometry( 80, 80 ),
-    new THREE.MeshPhongMaterial( { wireframe: false } )
-  );
-
-  plane.scale.set( 2, 2, 2 );
-  plane.position.set( 0, 200, -100 );
+//  var plane = new THREE.Mesh(
+//    new THREE.PlaneGeometry(80,80),
+//    new THREE.MeshPhongMaterial({wireframe: false})
+//  );
+//
+//
+//  plane.scale.set(2,2,2);
+//  plane.position.set(0,200,-100);
 
   camera.lookAt( plane.position );
 
@@ -33,11 +36,12 @@ Leap.loop()
 
   var controls = new THREE.OrbitControls( camera, renderer.domElement );
 
-    Leap.loopController.on( 'handFound' , function( hand ) {
-      document.getElementById( 'canvas' ).style.display = 'block';
-    } ).on( 'handLost', function( hand ) {
-      if ( Leap.loopController.frame( 0 ).hands.length === 0 ) {
-        document.getElementById( 'canvas' ).style.display = 'none';
-      }
-    } );
+  Leap.loopController.on( 'handFound' , function( hand ) {
+    document.getElementById( 'canvas' ).style.display = 'block';
+  } ).on( 'handLost', function( hand ) {
+    if ( Leap.loopController.frame( 0 ).hands.length === 0 ) {
+      document.getElementById( 'canvas' ).style.display = 'none';
+    }
+  } );
+
 }
