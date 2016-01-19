c4e5294b2ea98d03f3b1f7d7a36b2ac2a2aa9fc9
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-01-19 12:52:02 -0500

    user created obj animation tweek

--- public/javascript/leap.js
+++ public/javascript/leap.js
@@ -1,4 +1,88 @@
+
 function initLeap() {
+
+
+
+
+  function LeapExample(exampleID, initCallback, frameCallback) {
+    var self = this;
+    this.exampleID = exampleID;
+    this.initCallback = initCallback;
+    this.frameCallback = frameCallback;
+
+    this.serviceConnected = false;
+    this.leapConnected = false;
+    this.initialized = false;
+    this.controller = new Leap.Controller();
+
+    //Initialize example code and set onframe callback
+    this.init = function () {
+        self.exampleElement = document.getElementById(self.exampleID);
+        if (self.initCallback) self.initialized = self.initCallback();
+        self.controller.on("frame", self.forwardFrameCallback);
+    }
+
+    //Look for a valid frame to see if device is present
+    this.frameDetectedCallback = function() {
+        if(self.controller.frame(0).valid){
+           if (!self.initialized) self.init();
+           console.log("Device detected");
+           self.leapConnected = true;
+           self.exampleElement.style.visibility = "visible";
+           window.clearTimeout(self.timeout);
+           self.controller.removeListener("frame", self.frameDetectedCallback);
+        }
+    }
+
+    //Forward frames to example code
+    this.forwardFrameCallback = function(){
+         if (self.frameCallback) self.frameCallback(self.controller.frame(0));
+    }
+
+    //On connection to service, set a timeout to warn if valid frames
+    //aren't detected in a reasonable amount of time
+    this.controller.on('connect', function () {
+        console.log("Service connected");
+        serviceConnected = true;
+        self.timeout = window.setTimeout(function(){
+            console.log("Couldn't detect device");
+        }, 500);
+        self.controller.on("frame", self.frameDetectedCallback);
+    });
+
+    this.controller.on('disconnect', function () {
+        console.log("Service disconnected");
+        serviceConnected = false;
+    });
+
+    this.controller.on('deviceConnected', function () {
+        if (!self.initialized) self.init();
+        console.log("Device connected");
+        self.leapConnected = true;
+        self.exampleElement.style.visibility = "visible";
+    });
+
+    this.controller.on('deviceDisconnected', function () {
+        console.log("Device disconnected");
+        self.leapConnected = false;
+        self.exampleElement.style.visibility = "hidden";
+    });
+
+    this.controller.connect();
+}
+
+//Example example that uses the LeapExample class:
+
+var exampleInit = function () {
+    console.log("Init called")
+    return true;
+}
+
+var exampleFrame = function (frame) {
+    console.log("Frame: " + frame.id + " is " + (frame.valid ? "valid." : "invalid."));
+}
+
+var example = new LeapExample("thisLeapExample", exampleInit, exampleFrame);
 
 console.log( 'at setUpLeapLayer' );
 
@@ -10,14 +94,18 @@ console.log( 'at setUpLeapLayer' );
 //   console.log( 'z-index:', $( this ), 'is:', $( this ).css( 'z-index' ) );
 // } );
 
-// If the device is mobile then no physicakl device - hack
+// If the device is mobile then no physical device - hack
 
 var isInitiator = userContext.isSessionInitiator;
 console.log( 'isInitiator:', isInitiator );
 console.log( 'userContext...isInitiator:', userContext.isSessionInitiator );
 
-if ( userContext.mobile === false && isInitiator === true ) {
+if ( userContext.mobile === false ) {
   runHands();
+}
+
+if (userContext.participantState == 'peer') {
+  console.log( 'User State is peer' );
 }
 
 function runHands() {
