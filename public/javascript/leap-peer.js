d6fe0c3f687c5bed3e4ec2487d46c3a1c0803bf8
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-02-04 15:03:03 -0500

    leap collaboration

--- public/javascript/leap-peer.js
+++ public/javascript/leap-peer.js
@@ -109,7 +109,6 @@ function initLeapPeerHand() {
     container.style.position   = 'absolute';
     container.style.top        = '0px';
     container.style.left       = '0px';
-    container.style.zIndex = '200';
    // container.style.backgroundColor = 'transparent';
 
     document.body.appendChild( container );
