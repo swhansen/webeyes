86b8058d2b727fe84d9d220af8a2ace9c8311781
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-02-07 13:14:18 -0500

    leap collaboration

--- public/javascript/leap-focus.js
+++ public/javascript/leap-focus.js
@@ -19,7 +19,7 @@ function emitLeap( data ) {
     socketServer.emit( 'leapShare', JSON.stringify( data ), sessionId );
   }
 
-    controller.on( 'beforeFrameCreated', function( frameData ) { emitLeap (frameData); });
+ //   controller.on( 'beforeFrameCreated', function( frameData ) { emitLeap (frameData); });
 
     renderer = new THREE.WebGLRenderer( { canvas: leapfull, alpha: 1, antialias: true, clearColor: 0xffffff }  );
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -76,7 +76,7 @@ camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight
 
           var boneMesh = boneMeshes [ countBones ] || addMesh( boneMeshes );
 
-          console.log( bone );
+          emitLeap( bone );
           updateMesh( bone, boneMesh );
 
 
