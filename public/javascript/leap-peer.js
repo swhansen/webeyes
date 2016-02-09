7dab784d32e32a8f8a7f46257479de8dc3564a37
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-02-09 09:44:47 -0500

    leap collaboration

--- public/javascript/leap-peer.js
+++ public/javascript/leap-peer.js
@@ -63,7 +63,7 @@ var lCanvas = document.getElementById( 'leapcanvas' );
     [ 0xFF00FF , 0x3FEFF3 , 0x1FFB75 ],
     [ 0xDCFF55 , 0xAA6B9E , 0xFF6B75 ],
     [ 0xFFAA00 , 0xD0F7F3 , 0xFC8CD5 ],
-    [ 0xf7dda9 , 0xD0F7F3 , 0xFC8CD5 ],
+    [ 0x1078AC , 0xD0F7F3 , 0xFC8CD5 ],
   ];
 
   var geometries = [];
