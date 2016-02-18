f0d0c2421d15b46114d3ef2cd56f79cb0f84af0c
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-02-18 10:15:17 -0500

    leap collaboration

---
+++ public/javascript/leap-peer-1.js
@@ -0,0 +1,100 @@
+//
+// Implements a peers side Leap hand based on data from
+// a socket.io broadcast of raw JSON tracking data sent by leap-focus
+//
+
+function initLeapPeerHand() {
+
+//$( '*' ).filter( function() {
+//   return $( this ).css( 'z-index' ) >= 10;
+// } ).each( function() {
+//   console.log( 'z-index:', $( this ), 'is:', $( this ).css( 'z-index' ) );
+// } );
+
+ var leapFull = document.getElementById( 'leapfull' );
+
+    leapFull.style.width      = '100%';
+    leapFull.style.height     = '100%';
+    leapFull.style.position   = 'absolute';
+    leapFull.style.top        = '0px';
+    leapFull.style.left       = '0px';
+    leapFull.style.zIndex = 10;
+
+    var baseBoneRotation = ( new THREE.Quaternion ).setFromEuler( new THREE.Euler( 0, 0, Math.PI / 2 ) );
+    var armMeshes = [];
+    var boneMeshes = [];
+
+    var renderer, scene, camera, controls;
+
+    renderer = new THREE.WebGLRenderer( { canvas: leapfull, alpha: true }  );
+    renderer.setClearColor( 0xffffff, 0 );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 5000 );
+    camera.position.set( -500, 500, 500 );
+
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
+    controls.maxDistance = 1000;
+
+    scene = new THREE.Scene();
+  }
+
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+  }
+
+  function addMesh( meshes ) {
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshNormalMaterial();
+    var mesh = new THREE.Mesh( geometry, material );
+    meshes.push( mesh );
+    return mesh;
+  }
+
+  function updateMesh( bone, mesh ) {
+      mesh.position.fromArray( bone.center() );
+      mesh.setRotationFromMatrix( ( new THREE.Matrix4 ).fromArray( bone.matrix() ) );
+      mesh.quaternion.multiply( baseBoneRotation );
+      mesh.scale.set( bone.width, bone.width, bone.length );
+      scene.add( mesh );
+  }
+
+  function leapAnimate( data ) {
+
+    var frame = new Leap.Frame( data );
+    console.log('test frame:', myFrame );
+
+    var countBones = 0;
+    var countArms = 0;
+
+    armMeshes.forEach( function( item ) { scene.remove( item ); } );
+    boneMeshes.forEach( function( item ) { scene.remove( item ); } );
+
+    for ( var hand of frame.hands ) {
+      for ( var finger of hand.fingers ) {
+        for ( var bone of finger.bones ) {
+          if ( countBones++ === 0 ) { continue; }
+          var boneMesh = boneMeshes [ countBones ] || addMesh( boneMeshes );
+          updateMesh( bone, boneMesh );
+        }
+      }
+
+      var arm = hand.arm;
+      var armMesh = armMeshes [ countArms++ ] || addMesh( armMeshes );
+      updateMesh( arm, armMesh );
+      armMesh.scale.set( arm.width / 4, arm.width / 2, arm.length );
+    }
+
+    renderer.render( scene, camera );
+    controls.update();
+  }
+
+
+socketServer.on( 'leapShare', function( data ) {
+    frame = JSON.parse( data );
+//    animateTrackingData();
+    leapAnimate();
+    } );
+
