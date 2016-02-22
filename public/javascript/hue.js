f1d16c2bd82993d600379cafc29e84c120cf4d98
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-02-22 12:56:13 -0500

    hue light tweeks

--- public/javascript/hue.js
+++ public/javascript/hue.js
@@ -1,5 +1,5 @@
 
-var hueURL = 'http://10.0.1.24/api/4cca312bfd9d1976814b78d491ecd8b';
+var hueURL = 'http://10.0.1.2/api/4cca312bfd9d1976814b78d491ecd8b';
 
 var setHueLightState = function( lightNo, state ) {
 
