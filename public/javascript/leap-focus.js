c1c5379d509302762bb9e14439a48c9a966cac01
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-02-28 21:11:12 -0500

    hue light utils

--- public/javascript/leap-focus.js
+++ public/javascript/leap-focus.js
@@ -219,7 +219,7 @@ function updateHandSphere( palmCenter, radius, interactionBox ) {
 //  Logic for hue grab-relase interaction
 //
 
-      if ( hand.pinchStrength > 0 ) {
+      if ( hand.pinchStrength == 1 ) {
         var pinchFinger = findPinchingFingerType( hand );
           if ( pinchFinger.type == 1 ) {
         updateHueText( hand.sphereCenter );
