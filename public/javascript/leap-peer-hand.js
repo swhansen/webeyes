70678f52f827b6ee5f8be2f646831f5e0acb99e1
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-03-08 16:31:16 -0500

    hue light utils

--- public/javascript/leap-peer-hand.js
+++ public/javascript/leap-peer-hand.js
@@ -116,7 +116,7 @@ function arObjMover() {
     var mouseVector = new THREE.Vector3( ( ev._x / window.innerWidth ) * 2 - 1,
                             -( ev._y / window.innerHeight ) * 2 + 1, 1.0 );
 
-
+console.log( 'mouseVector:', mouseVector );
 
     //var foo = raycaster.setFromCamera( mouseVector, camera );
 
