f1890bfa8102e0cb5a293c4a2e72bc70cccf75c8
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-03-09 21:30:48 -0500

    hue light utils

--- public/javascript/leap-peer-hand.js
+++ public/javascript/leap-peer-hand.js
@@ -120,7 +120,7 @@ function arObjMover() {
     if ( tool.down && selectState ) {
   //  console.log( 'moving object:', ev.x, ev.y );
 
-    var leapX = ( ev._x / window.innerWidth * 2 - 1 ) * 278.5;
+    var leapX = ( ev._x / window.innerWidth * 2 + 1 ) * 278.5;
     var leapY = -( ev._y / window.innerHeight * 2 + 1) * 278.5;
 
 
