29c844169751715d0453bd3bbbcb5fd28c030e96
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-03-18 16:43:23 -0400

    AR object collab interaction

--- public/javascript/leap-focus.js
+++ public/javascript/leap-focus.js
@@ -268,7 +268,7 @@ function updateHandSphere( data ) {
     scene.remove( peerSphere );
 
     for ( var hand of frame.hands ) {
-      if ( hand.grabStength < 0.05 ) {
+      if ( hand.grabStrength < 0.05 ) {
       handState.iotSelectEligible = true;
       console.log( 'handState:', handState);
     }
