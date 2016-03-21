1e74c7c347dd5ef04c7595b115d114e5a4533648
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-03-21 13:35:35 -0400

    AR object collab interaction

--- public/javascript/leap-focus.js
+++ public/javascript/leap-focus.js
@@ -185,13 +185,33 @@ function updatePeerSphere( data ) {
     peerSphere.position.y = data.position[1];
     peerSphere.position.z = data.position[2];
 
-   // peerSphere.material.color = data.color;
-    peerSphere.material.color.r = data.color[0];
-    peerSphere.material.color.g = data.color[1];
-    peerSphere.material.color.b = data.color[2];
+    peerSphere.material.color = data.color;
+    //peerSphere.material.color.r = data.color[0];
+    //peerSphere.material.color.g = data.color[1];
+    //peerSphere.material.color.b = data.color[2];
     peerSphere.visible = true;
 
-    if ( data.setHueState ) {
+  // if ( data.setHueState ) {
+
+  //   var normalizedSphere = normalizePoint( data.position );
+
+  //   var hueXY = getXYPointFromRGB(
+  //               normalizedSphere[0] * 255,
+  //               normalizedSphere[1] * 255,
+  //               normalizedSphere[2] * 255 );
+
+  //     hueSetLightStateXY( 1, true, [ hueXY.x, hueXY.y ], 100 );
+  //     iotLightOn.play();
+  //   //  scene.remove( peerSphere );
+
+  //     handState.inChooseState = false;
+  //     handState.iotSelectEligible = false;
+  //   }
+    }
+
+if ( data.operation === 'mouseUp' ) {
+
+  if ( data.setHueState ) {
 
       var normalizedSphere = normalizePoint( data.position );
 
@@ -202,12 +222,15 @@ function updatePeerSphere( data ) {
 
         hueSetLightStateXY( 1, true, [ hueXY.x, hueXY.y ], 100 );
         iotLightOn.play();
-        scene.remove( peerSphere );
+      //  scene.remove( peerSphere );
 
         handState.inChooseState = false;
         handState.iotSelectEligible = false;
       }
-    }
+
+scene.remove( peerSphere );
+}
+
 }
 
 function onHandFound() {
