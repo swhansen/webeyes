23713884e16e34e615f3aebd812b78fd714b3034
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-03-22 13:44:25 -0400

    AR object collab interaction

--- public/javascript/leap-focus.js
+++ public/javascript/leap-focus.js
@@ -10,9 +10,9 @@ var iotIncrement = new Audio( 'audio/button-19.wav');
 var iotLightOn = new Audio( 'audio/button-17.wav');
 var iotLightOff = new Audio( 'audio/button-47.wav');
 
- var leapFull = document.getElementById( 'leapfull' );
- //var leapCanvas = document.getElementById( '')
- document.getElementById( 'leapfull' ).className = 'canvascenter';
+ var leapFull = document.getElementById( 'leappanel' );
+ var leapCanvas = document.getElementById( 'leapfull')
+ document.getElementById( 'leappane' ).className = 'canvascenter';
 
     leapFull.style.width      = '100%';
     leapFull.style.height     = '100%';
