9b796d5606c271b3ec9259a863b0db151939ed87
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-03-23 20:01:13 -0400

    AR object collab interaction

--- public/javascript/leap-peer-hand.js
+++ public/javascript/leap-peer-hand.js
@@ -14,8 +14,8 @@ socketServer.on( 'leapSphere', function( data ) {
  var leapFull = document.getElementById( 'leapfull');
  document.getElementById( 'leappane' ).className = 'leapcenter';
 
- leapFull.style.width = '100%';
- leapFull.style.height = '100%';
+// leapFull.style.width = '100%';
+// leapFull.style.height = '100%';
 
  leapFull.width =  leapFull.offsetWidth;
  leapFull.height = leapFull.offsetHeight;
