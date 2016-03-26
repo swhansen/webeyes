75943523a929f0f44e21c52aaec2b75ff885369e
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-03-26 13:41:33 -0400

    Layer Manager

--- public/javascript/ui-manager.js
+++ public/javascript/ui-manager.js
@@ -36,24 +36,35 @@ function getDomPointerStatus( list ) {
 
 console.log( 'domPointerStatus:', getDomPointerStatus( layerList ) );
 
-function getDomZindex( list ) {
+function getDomZindex() {
 var obj = {};
-_.forEach( list, function( key ) {
+//_.forEach( list, function( key ) {
+//
+//  var elem = document.getElementById( key );
+//
+//  //var theCSSprop = document.getElementById( key ).style.zIndex;
+//
+//  var theCSSprop = $( key ).css( 'z-index' );
+//
+//  //var theCSSprop = window.getComputedStyle( elem, null ).getPropertyValue( 'zIndex' );
+//
+//  obj[ key ] = theCSSprop;
+//  } );
 
-  var elem = document.getElementById( key );
 
-  //var theCSSprop = document.getElementById( key ).style.zIndex;
+$( '*' ).filter( function() {
+   return $( this ).css( 'z-index' ) >= 10;
+ } ).each( function() {
+  // console.log( 'z-index:', $( this ), 'is:', $( this ).css( 'z-index' ) );
 
-  var theCSSprop = $( key ).css( 'z-index' );
+  obj[ $( this ) ] = $( this ).css( 'z-index' );
 
-  //var theCSSprop = window.getComputedStyle( elem, null ).getPropertyValue( 'zIndex' );
+ } );
 
-  obj[ key ] = theCSSprop;
-  } );
   return obj;
 }
 
-console.log( 'domZindex:', getDomZindex( layerList ) );
+console.log( 'domZindex:', getDomZindex() );
 
 //
 // Sticky menus
