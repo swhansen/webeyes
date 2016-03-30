948fd17c75e3220c15dd5f98f5091b27064ce0eb
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-03-30 08:56:16 -0400

    VR mode initial

--- public/javascript/vr.js
+++ public/javascript/vr.js
@@ -70,7 +70,7 @@ function setUpArLayer( participantState ) {
   renderer.setSize( box0Width, box0Width );
   renderer.setClearColor( 0x000000, 0 );
 
-  controls = new THREE.PointerLockControls( camera );
+  controls = new THREE.MouseControls( camera, renderer.domElement );
   controls.enabled = true;
 
  // controls.maxDistance = 5000;
@@ -301,7 +301,7 @@ animate();
 
 function animate() {
         requestAnimationFrame( animate );
-      // controls.update(); // required if controls.enableDamping = true, or if controls.autoRotate = true
+       controls.update(); // required if controls.enableDamping = true, or if controls.autoRotate = true
        // stats.update();
         render();
       }
