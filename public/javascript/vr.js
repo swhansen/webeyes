77e978a35deb44af950f01b6fd9d9080a3217427
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-03-30 09:11:02 -0400

    VR mode initial

--- public/javascript/vr.js
+++ public/javascript/vr.js
@@ -78,9 +78,6 @@ function setUpArLayer( participantState ) {
  // controls.rotateSpeed = 5.0;
   //controls.autoRotate = true;
 
-
-
-
   var geometryCube1 = new THREE.BoxGeometry( 0.5, 0.5, 0.5, 2, 2, 2 );
   var geometryCube2 = new THREE.BoxGeometry( 0.8, 0.8, 0.8 );
   var geometrySphere = new THREE.SphereGeometry( 0.15, 16, 16 );
@@ -181,43 +178,43 @@ function setUpArLayer( participantState ) {
 
 // Sword guy
 
-//  loader.load( '../armodels/knight.js', function( geometry, materials ) {
-//       createSwordGuy( geometry, materials, 0, -15.0, 65.0, 3.0 );
-//     } );
-//  function createSwordGuy( geometry, materials, x, y, z, s ) {
-//     geometry.computeBoundingBox();
-//     var bb = geometry.boundingBox;
-//     for ( var i = 0; i < materials.length; i++ ) {
-//       var m = materials[ i ];
-//       m.skinning = true;
-//       m.morphTargets = true;
-//       m.specular.setHSL( 0, 0, 0.1 );
-//       m.color.setHSL( 0.6, 0, 0.6 );
-//     }
-//     swordGuyMesh = new THREE.SkinnedMesh( geometry, new THREE.MeshFaceMaterial( materials ) );
-//     swordGuyMesh.position.set( x, y - bb.min.y * s, z );
-//     swordGuyMesh.scale.set( s, s, s );
-//     swordGuyMesh.rotation.y =  -Math.PI;
-//     swordGuyMesh.name = 'swordGuy';
-//     swordGuyMesh.userData.objectType =  'swordGuyMesh';
-//     swordGuyMesh.userData.isAnimated = false;
-//     swordGuyMesh.userData.isUserCreated = false;
-//     swordGuyMesh.userData.isSelectable = true;
-//     swordGuyMesh.userData.createdBy = 'system';
-//     scene.add( swordGuyMesh );
-//  //   swordGuyMesh.castShadow = true;
-//  //   swordGuyMesh.receiveShadow = true;
-//     helper = new THREE.SkeletonHelper( swordGuyMesh );
-//     helper.material.linewidth = 3;
-//     helper.visible = false;
-//     scene.add( helper );
-//     var clipMorpher = THREE.AnimationClip.CreateFromMorphTargetSequence( 'facialExpressions', swordGuyMesh.geometry.morphTargets, 3 );
-//     var clipBones = geometry.animations[0];
-//     mixer = new THREE.AnimationMixer( swordGuyMesh );
-//     mixer.addAction( new THREE.AnimationAction( clipMorpher ) );
-//     mixer.addAction( new THREE.AnimationAction( clipBones ) );
-//     arSelectObjectArray.push( swordGuyMesh );
-//   }
+  loader.load( '../armodels/knight.js', function( geometry, materials ) {
+       createSwordGuy( geometry, materials, 0, -15.0, 65.0, 3.0 );
+     } );
+  function createSwordGuy( geometry, materials, x, y, z, s ) {
+     geometry.computeBoundingBox();
+     var bb = geometry.boundingBox;
+     for ( var i = 0; i < materials.length; i++ ) {
+       var m = materials[ i ];
+       m.skinning = true;
+       m.morphTargets = true;
+       m.specular.setHSL( 0, 0, 0.1 );
+       m.color.setHSL( 0.6, 0, 0.6 );
+     }
+     swordGuyMesh = new THREE.SkinnedMesh( geometry, new THREE.MeshFaceMaterial( materials ) );
+     swordGuyMesh.position.set( x, y - bb.min.y * s, z );
+     swordGuyMesh.scale.set( s, s, s );
+     swordGuyMesh.rotation.y =  -Math.PI;
+     swordGuyMesh.name = 'swordGuy';
+     swordGuyMesh.userData.objectType =  'swordGuyMesh';
+     swordGuyMesh.userData.isAnimated = false;
+     swordGuyMesh.userData.isUserCreated = false;
+     swordGuyMesh.userData.isSelectable = true;
+     swordGuyMesh.userData.createdBy = 'system';
+     scene.add( swordGuyMesh );
+  //   swordGuyMesh.castShadow = true;
+  //   swordGuyMesh.receiveShadow = true;
+     helper = new THREE.SkeletonHelper( swordGuyMesh );
+     helper.material.linewidth = 3;
+     helper.visible = false;
+     scene.add( helper );
+     var clipMorpher = THREE.AnimationClip.CreateFromMorphTargetSequence( 'facialExpressions', swordGuyMesh.geometry.morphTargets, 3 );
+     var clipBones = geometry.animations[0];
+     mixer = new THREE.AnimationMixer( swordGuyMesh );
+     mixer.addAction( new THREE.AnimationAction( clipMorpher ) );
+     mixer.addAction( new THREE.AnimationAction( clipBones ) );
+     arSelectObjectArray.push( swordGuyMesh );
+   }
 
 // hue light control objects
 
