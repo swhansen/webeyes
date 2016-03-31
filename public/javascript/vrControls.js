4d2b3cf62dcd3b79ad90ab8a645e235125bcd700
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-03-31 09:49:28 -0400

    VR mode initial

--- public/javascript/vrControls.js
+++ public/javascript/vrControls.js
@@ -1,10 +1,5 @@
-/**
- * @author dmarcos / http://github.com/dmarcos
- *
- * This controls allow to change the orientation of the camera using the mouse
- */
 
-WEBEYES.MouseControls = function ( object ) {
+WEBEYES.MouseControls = function( object ) {
 
   var scope = this;
   var PI_2 = Math.PI / 2;
@@ -12,7 +7,7 @@ WEBEYES.MouseControls = function ( object ) {
     x: new THREE.Quaternion(),
     y: new THREE.Quaternion()
   };
-  var object = object;
+  //var object = object;
   var xVector = new THREE.Vector3( 1, 0, 0 );
   var yVector = new THREE.Vector3( 0, 1, 0 );
 
@@ -20,49 +15,47 @@ WEBEYES.MouseControls = function ( object ) {
 
   var onMouseDown = function( event ) {
 
-    if ( scope.enabled === false ) return;
+    if ( scope.enabled === false ) { return; }
 
     mouseDown = true;
-    //document.addEventListener( 'mousemove', onMouseMove, false );
-    }
+    };
 
-  var onMouseMove = function ( event ) {
-    if ( scope.enabled === false ) return;
-  //  if ( mouseDown === false ) return;
+  var onMouseMove = function( event ) {
+
+    if ( scope.enabled === false ) { return; }
 
     if ( mouseDown ) {
 
-    var orientation = scope.orientation;
+      var orientation = scope.orientation;
 
-    var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-    var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+      var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+      var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
 
-    orientation.y += movementX * 0.005;
-    orientation.x += movementY * 0.005;
-    orientation.x = Math.max( -PI_2, Math.min( PI_2, orientation.x ) );
+      orientation.y += movementX * 0.005;
+      orientation.x += movementY * 0.005;
+      orientation.x = Math.max( -PI_2, Math.min( PI_2, orientation.x ) );
     }
   };
 
   var onMouseUp = function( event ) {
       mouseDown = false;
-    }
+    };
 
   this.enabled = true;
 
   this.orientation = {
     x: 0,
-    y: 0,
+    y: 0
   };
 
   this.update = function() {
 
-    if ( this.enabled === false ) return;
+    if ( this.enabled === false ) { return; }
 
     mouseQuat.x.setFromAxisAngle( xVector, this.orientation.x );
     mouseQuat.y.setFromAxisAngle( yVector, this.orientation.y );
-    object.quaternion.copy(mouseQuat.y).multiply(mouseQuat.x)
+    object.quaternion.copy( mouseQuat.y ).multiply( mouseQuat.x );
     return;
-
   };
 
   document.addEventListener( 'mousemove', onMouseMove, false );
