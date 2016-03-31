a3351865b3b72b62bc677a7dfb75a53d7d726d3d
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-03-31 08:45:32 -0400

    VR mode initial

--- public/javascript/vrControls.js
+++ public/javascript/vrControls.js
@@ -13,65 +13,30 @@ WEBEYES.MouseControls = function ( object ) {
     y: new THREE.Quaternion()
   };
   var object = object;
-  var moveState = false;
   var xVector = new THREE.Vector3( 1, 0, 0 );
   var yVector = new THREE.Vector3( 0, 1, 0 );
 
-  var mouseDown = false;
-
-
-  var onMouseDown = function( event ) {
-
-    mouseDown = true;
+  var onMouseMove = function ( event ) {
 
     if ( scope.enabled === false ) return;
 
     var orientation = scope.orientation;
+
     var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
     var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+
     orientation.y += movementX * 0.005;
     orientation.x += movementY * 0.005;
+
     orientation.x = Math.max( - PI_2, Math.min( PI_2, orientation.x ) );
 
-    document.addEventListener( 'mousemove', onMouseMove, false );
-    document.addEventListener( 'mouseup', onMouseUp, false );
   };
-
-  var onMouseMove = function( event ) {
-
-
-
-    if ( scope.enabled === false ) return;
-
-    if ( mouseDown === true ) {
-
-    var orientation = scope.orientation;
-    var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-    var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
-    orientation.y += movementX * 0.005;
-    orientation.x += movementY * 0.005;
-    orientation.x = Math.max( - PI_2, Math.min( PI_2, orientation.x ) );
-    console.log( 'vrC-down:', orientation.x, orientation.y );
-  }
-};
-
-var onMouseUp = function( event ) {
-
-  if ( mouseDown === true ) {
-
-    document.removeEventListener( 'mousemove', onMouseMove, false );
-    document.removeEventListener( 'mouseup', onMouseUp, false );
-    scope.dispatchEvent( endEvent );
-    mouseDown = false;
-  }
-};
-
 
   this.enabled = true;
 
   this.orientation = {
     x: 0,
-    y: 0
+    y: 0,
   };
 
   this.update = function() {
@@ -80,11 +45,11 @@ var onMouseUp = function( event ) {
 
     mouseQuat.x.setFromAxisAngle( xVector, this.orientation.x );
     mouseQuat.y.setFromAxisAngle( yVector, this.orientation.y );
-    object.quaternion.copy( mouseQuat.y ).multiply( mouseQuat.x );
+    object.quaternion.copy(mouseQuat.y).multiply(mouseQuat.x)
     return;
 
   };
 
-  document.addEventListener( 'mousedown', onMouseDown, false );
+  document.addEventListener( 'mousemove', onMouseMove, false );
 
 };
