6ef759b2af17abb339f5db63763bfbf136b25eee
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-04-08 09:34:34 -0400

    VR mode initial

--- public/javascript/vrControls.js
+++ public/javascript/vrControls.js
@@ -56,6 +56,7 @@ WEBEYES.MouseControls = function( object ) {
     mouseQuat.x.setFromAxisAngle( xVector, this.orientation.x );
     mouseQuat.y.setFromAxisAngle( yVector, this.orientation.y );
     object.quaternion.copy( mouseQuat.y ).multiply( mouseQuat.x );
+    console.log( 'vrControls:', object );
     return;
   };
 
