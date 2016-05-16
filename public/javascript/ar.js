9ee1ae1ae7784505707f75c125681a01f3fdeab9
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-05-16 13:20:39 -0400

    VR mode initial

--- public/javascript/ar.js
+++ public/javascript/ar.js
@@ -585,7 +585,7 @@ function arConnectionController( participantState ) {
    animateArObjects();
    renderer.render( scene, vrDrivenCamera );
    requestAnimationFrame( connectToVrController );
-   console.log('camera:', vrDrivenCamera );
+   //console.log('camera:', vrDrivenCamera );
  }
 
 // function connectToVrBroadcast() {
