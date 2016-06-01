e6bb991bae28af138674d86bebbb2b6f8737b10f
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-06-01 13:44:25 -0400

    AR VR collaborative mode interaction refine

--- public/javascript/ar.js
+++ public/javascript/ar.js
@@ -559,7 +559,7 @@ function arConnectionController( participantState ) {
 
   vrBroadcastCameraControls = new WEBEYES.BroadcastVrControls( vrBroadcastDrivenCamera );
 
-  arConnectionController( participantState );
+  //arConnectionController( participantState );
 
   function animateArObjects() {
 
