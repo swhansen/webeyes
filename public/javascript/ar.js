76ea6b2f1fedfe53bc7e81e6d3f8c7a1689aa48c
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-06-13 19:34:30 -0400

    AR VR collaborative mode interaction refine

--- public/javascript/ar.js
+++ public/javascript/ar.js
@@ -851,7 +851,7 @@ $( function() {
          arShareData.arObjectOpacity = selectedObject.material.opacity;
          arShareData.name = selectedObject.name;
 
-         animateArObjects();
+        // animateArObjects();
 
          emitArObject( arShareData );
          return;
@@ -883,7 +883,8 @@ $( function() {
      if ( intersects[0].object.name === 'knot' ) {
          isAnimateKnot = !isAnimateKnot;
 
-         animateArObjects();
+        // animateArObjects();
+
          return;
      }
 
@@ -906,7 +907,7 @@ $( function() {
           arShareData.rotation = intersects[0].object.rotation;
           arShareData.color = intersects[0].object.material.color;
 
-          animateArObjects();
+          // animateArObjects();
 
           emitArObject( arShareData );
      }
