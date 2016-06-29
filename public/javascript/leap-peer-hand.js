dad935909e1dc833cde480c4b29a3656bd8af6fd
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-06-29 14:01:09 -0400

    Leap peer iot integ

--- public/javascript/leap-peer-hand.js
+++ public/javascript/leap-peer-hand.js
@@ -157,8 +157,8 @@ function arObjMover() {
 
 // reverse the sign since peer hand is reversed
 
-      var mouseSphereX = -( ev._x  / box0Width * 2 - 1 ) * 278.5;
-      var mouseSphereY = ( ev._y  / box0Height * 2 - 1 ) * 278.5;
+      var mouseSphereX = ( ev._x  / box0Width * 2 - 1 ) * 278.5;
+      var mouseSphereY = -( ev._y  / box0Height * 2 - 1 ) * 278.5;
 
       var spherePos = [ mouseSphereX, mouseSphereY, 0 ];
       peerSphere.position.x = mouseSphereX;
