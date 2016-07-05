aad39957bf6cbe673658de7423968bea15dc9eef
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-07-05 15:59:33 -0400

    Leap peer iot integ

--- public/javascript/button-code.js
+++ public/javascript/button-code.js
@@ -60,6 +60,10 @@ userContext.modMeState = true;
 
     userContext.uistate = 'vr';
 
+    for ( button in uiStructure.structure ) {
+      $( uiStructure.structure[button].mainButton ).fadeOut( 1000 );
+    }
+
     document.getElementById( 'devmeButton' ).style.visibility = 'hidden';
     document.getElementById( 'modmeButton' ).style.visibility = 'hidden';
    // document.getElementById( 'arMainButton' ).style.visibility = 'hidden';
