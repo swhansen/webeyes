59039de041a03a8f1310626059f15c01ec67243b
Steve Hansen <stevehansen@Steves-MacBook-Air-2.local>
2016-11-14 09:51:00 -0500

    dynamically load AR world

--- public/javascript/ar.js
+++ public/javascript/ar.js
@@ -562,140 +562,140 @@ $.getScript( pathToWorld, function() {
 //
 // ... end AR world model
 
-function arConnectionController() {
-
-  socketServer.removeAllListeners( 'arObjectShare' );
-
-//   Set up the camera drivers and connection feed
-//   Based on participantState(focus or peer)
-//    focus - device sensors
-//    peer - broadcast feed sensors
-//    ar - sensor driven
-//    vr - mouse driven
-
-  if ( userContext.participantState === 'focus' && userContext.mode === 'vr' ) {
-    cameraDriver = vrDrivenCamera;
-    vrDrivenCamera.lookAt( scene.position );
-    connectToVrController();
-    socketServer.on( 'arObjectShare', function( data ) {
-           receiveArObject( data );
-      } );
-  }
-
-    if ( userContext.participantState === 'peer' && userContext.mode === 'vr' ) {
-      cameraDriver = vrBroadcastDrivenCamera;
-      vrBroadcastDrivenCamera.lookAt( scene.position );
-      connectToVrBroadcast();
-      socketServer.on( 'arObjectShare', function( data ) {
-           receiveArObject( data );
-      } );
-    }
-
-  if ( userContext.participantState === 'focus' && userContext.mode === 'ar' ) {
-      cameraDriver = sensorDrivenCamera;
-      sensorDrivenCamera.lookAt( scene.position );
-      connectToDeviceSensors();
-      socketServer.on( 'arObjectShare', function( data ) {
-           receiveArObject( data );
-      } );
-      }
-
-  if ( userContext.participantState === 'peer' && userContext.mode === 'ar' ) {
-      cameraDriver = broadcastDrivenCamera;
-      broadcastDrivenCamera.lookAt( scene.position );
-      connectToBroadcastSensors();
-      socketServer.on( 'arObjectShare', function( data ) {
-           receiveArObject( data );
-        } );
-      }
-}
-
-// end  arConnectionController
-
-// Attach the cameras to orientation provider
-//  - sensors for a mobile initiator ( ar mode )
-//  - mouse x-y ( VR mode )
-//  - broadcast for all peers
-
-// sensorCameraControls = new THREE.DeviceOrientationControls( sensorDrivenCamera );
-// broadcastCameraControls = new WEBEYES.BroadcastOrientationControls( broadcastDrivenCamera );
-// vrDrivenCameraControls = new WEBEYES.MouseControls( vrDrivenCamera );
-// vrBroadcastCameraControls = new WEBEYES.BroadcastVrControls( vrBroadcastDrivenCamera );
-
-  function animateArObjects() {
-
-    var dt = clock.getDelta();
-    step += dt;
-
-    var foo = clock.getElapsedTime() - clock.startTime;
-
-    sphere.position.x =  1.4 + ( 0.8 * ( Math.cos( foo ) ) ) ;
-    sphere.position.y = -0.2 + ( 0.9 * Math.abs( Math.sin( foo ) ) );
-
-    knot.position.y = -0.22 + ( 1.4 * Math.abs( Math.sin( foo ) ) );
-
-    if ( isAnimateKnot === true ) {
-        knot.rotation.y += 0.03;
-        knot.rotation.z += 0.03;
-        knot.position.z = -5.0 + ( -45.0 * Math.abs( Math.sin( foo ) ) );
-    }
-
-    if ( isAnimateSheep === true ) {
-        sheep.rotation.z += dt * 2;
-    }
-
-// Flying  Pig
-    if ( flyingPig !== undefined ) {
-      pivotPoint.rotation.y += dt * 1.0;
-    }
-
-// Sword Guy
-    if ( isAnimateSwordGuy === true ) {
-         mixer.update( dt );
-          helper.update();
-    }
-
-// User Created Objects - as of now only torus
-
-    for ( var i = 0; i < arSelectObjectArray.length; i++ ) {
-        if ( arSelectObjectArray[i].userData.objectType === 'bagel' &&
-              arSelectObjectArray[i].userData.isAnimated === true ) {
-          arSelectObjectArray[i].rotation.y += dt * 1.0;
-        }
-    }
-  }
-
- function connectToVrController() {
-   vrDrivenCameraControls.update();
-   animateArObjects();
-   renderer.render( scene, vrDrivenCamera );
-   requestAnimationFrame( connectToVrController );
- }
-
- function connectToVrBroadcast() {
-   vrBroadcastCameraControls.update();
-   animateArObjects();
-   renderer.render( scene, vrBroadcastDrivenCamera );
-   requestAnimationFrame( connectToVrBroadcast );
- }
-
- function connectToDeviceSensors() {
-   sensorCameraControls.update();
-   animateArObjects();
-   renderer.render( scene, sensorDrivenCamera );
-   requestAnimationFrame( connectToDeviceSensors );
-   }
-
- function connectToBroadcastSensors() {
-   broadcastCameraControls.update();
-   animateArObjects();
-   renderer.render( scene, broadcastDrivenCamera );
-   requestAnimationFrame( connectToBroadcastSensors );
-   }
-
- arConnectionController();
-
-  }
+//  function arConnectionController() {
+//
+//    socketServer.removeAllListeners( 'arObjectShare' );
+//
+//  //   Set up the camera drivers and connection feed
+//  //   Based on participantState(focus or peer)
+//  //    focus - device sensors
+//  //    peer - broadcast feed sensors
+//  //    ar - sensor driven
+//  //    vr - mouse driven
+//
+//    if ( userContext.participantState === 'focus' && userContext.mode === 'vr' ) {
+//      cameraDriver = vrDrivenCamera;
+//      vrDrivenCamera.lookAt( scene.position );
+//      connectToVrController();
+//      socketServer.on( 'arObjectShare', function( data ) {
+//             receiveArObject( data );
+//        } );
+//    }
+//
+//      if ( userContext.participantState === 'peer' && userContext.mode === 'vr' ) {
+//        cameraDriver = vrBroadcastDrivenCamera;
+//        vrBroadcastDrivenCamera.lookAt( scene.position );
+//        connectToVrBroadcast();
+//        socketServer.on( 'arObjectShare', function( data ) {
+//             receiveArObject( data );
+//        } );
+//      }
+//
+//    if ( userContext.participantState === 'focus' && userContext.mode === 'ar' ) {
+//        cameraDriver = sensorDrivenCamera;
+//        sensorDrivenCamera.lookAt( scene.position );
+//        connectToDeviceSensors();
+//        socketServer.on( 'arObjectShare', function( data ) {
+//             receiveArObject( data );
+//        } );
+//        }
+//
+//    if ( userContext.participantState === 'peer' && userContext.mode === 'ar' ) {
+//        cameraDriver = broadcastDrivenCamera;
+//        broadcastDrivenCamera.lookAt( scene.position );
+//        connectToBroadcastSensors();
+//        socketServer.on( 'arObjectShare', function( data ) {
+//             receiveArObject( data );
+//          } );
+//        }
+//  }
+//
+//  // end  arConnectionController
+//
+//  // Attach the cameras to orientation provider
+//  //  - sensors for a mobile initiator ( ar mode )
+//  //  - mouse x-y ( VR mode )
+//  //  - broadcast for all peers
+//
+//  // sensorCameraControls = new THREE.DeviceOrientationControls( sensorDrivenCamera );
+//  // broadcastCameraControls = new WEBEYES.BroadcastOrientationControls( broadcastDrivenCamera );
+//  // vrDrivenCameraControls = new WEBEYES.MouseControls( vrDrivenCamera );
+//  // vrBroadcastCameraControls = new WEBEYES.BroadcastVrControls( vrBroadcastDrivenCamera );
+//
+//    function animateArObjects() {
+//
+//      var dt = clock.getDelta();
+//      step += dt;
+//
+//      var foo = clock.getElapsedTime() - clock.startTime;
+//
+//      sphere.position.x =  1.4 + ( 0.8 * ( Math.cos( foo ) ) ) ;
+//      sphere.position.y = -0.2 + ( 0.9 * Math.abs( Math.sin( foo ) ) );
+//
+//      knot.position.y = -0.22 + ( 1.4 * Math.abs( Math.sin( foo ) ) );
+//
+//      if ( isAnimateKnot === true ) {
+//          knot.rotation.y += 0.03;
+//          knot.rotation.z += 0.03;
+//          knot.position.z = -5.0 + ( -45.0 * Math.abs( Math.sin( foo ) ) );
+//      }
+//
+//      if ( isAnimateSheep === true ) {
+//          sheep.rotation.z += dt * 2;
+//      }
+//
+//  // Flying  Pig
+//      if ( flyingPig !== undefined ) {
+//        pivotPoint.rotation.y += dt * 1.0;
+//      }
+//
+//  // Sword Guy
+//      if ( isAnimateSwordGuy === true ) {
+//           mixer.update( dt );
+//            helper.update();
+//      }
+//
+//  // User Created Objects - as of now only torus
+//
+//      for ( var i = 0; i < arSelectObjectArray.length; i++ ) {
+//          if ( arSelectObjectArray[i].userData.objectType === 'bagel' &&
+//                arSelectObjectArray[i].userData.isAnimated === true ) {
+//            arSelectObjectArray[i].rotation.y += dt * 1.0;
+//          }
+//      }
+//    }
+//
+//   function connectToVrController() {
+//     vrDrivenCameraControls.update();
+//     animateArObjects();
+//     renderer.render( scene, vrDrivenCamera );
+//     requestAnimationFrame( connectToVrController );
+//   }
+//
+//   function connectToVrBroadcast() {
+//     vrBroadcastCameraControls.update();
+//     animateArObjects();
+//     renderer.render( scene, vrBroadcastDrivenCamera );
+//     requestAnimationFrame( connectToVrBroadcast );
+//   }
+//
+//   function connectToDeviceSensors() {
+//     sensorCameraControls.update();
+//     animateArObjects();
+//     renderer.render( scene, sensorDrivenCamera );
+//     requestAnimationFrame( connectToDeviceSensors );
+//     }
+//
+//   function connectToBroadcastSensors() {
+//     broadcastCameraControls.update();
+//     animateArObjects();
+//     renderer.render( scene, broadcastDrivenCamera );
+//     requestAnimationFrame( connectToBroadcastSensors );
+//     }
+//
+//   arConnectionController();
+//
+//    }
 
   // end setup AR Layer
 
