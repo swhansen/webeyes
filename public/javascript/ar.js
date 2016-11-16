2906ae1ef32fb9916e25d2259647f6741c57c5ac
Steve Hansen <stevehansen@Steves-MacBook-Air-2.local>
2016-11-15 22:06:01 -0500

    dynamic models

--- public/javascript/ar.js
+++ public/javascript/ar.js
@@ -95,7 +95,7 @@ function loadAr() {
   var $arcanvaspane =  $( '<div>', {id: 'arcanvaspane', class: 'canvascenter' } );
   var $arcanvas =  $( '<canvas>', {id: 'arcanvas', class: 'canvasorient' } );
 
-  $( 'body' ).append( $arcanvaspane, [ $arcanvas ] );
+  $( 'body' ).appendTo( $arcanvaspane, [ $arcanvas ] );
 
   $( '#arcanvas' ).click.off;
 
