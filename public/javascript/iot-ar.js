9eb6fe94ede0b16cd6513543e8afaca372ee1fee
Steve Hansen <stevehansen@Steves-MacBook-Air-2.local>
2016-11-16 13:51:48 -0500

    dynamic models

--- public/javascript/iot-ar.js
+++ public/javascript/iot-ar.js
@@ -4,6 +4,15 @@ function  loadIotAr() {
 
 console.log( 'At loadIotAR' );
 
+ var hueGeometrySphere = new THREE.SphereGeometry( 0.2, 16, 16 );
+
+    hueLightmaterial1 = new THREE.MeshPhongMaterial ( {
+      color: 0xff00ff,
+      shininess: 66,
+      opacity:0.5,
+      transparent: true
+  } );
+
     hueLightmaterial2 = new THREE.MeshPhogMterial ( {
       color: 0xff00ff,
       shininess: 66,      opacity:0.5,
