5db0486800100cfa2afeab2baf2bb40f471d0a16
Steve Hansen <stevehansen@Steves-MacBook-Air-2.local>
2016-11-29 09:39:05 -0500

    radial menu refimement

--- public/javascript/static-multiparty.js
+++ public/javascript/static-multiparty.js
@@ -824,7 +824,7 @@ function showTextEntry() {
     document.getElementById( 'textEntryButton' ).style.display = 'none';
     document.getElementById( 'textentryField' ).focus();
     moveLayertoTop( 'textentryBox' );
-    moveLayertoTop( 'textEntryButton' );
+   // moveLayertoTop( 'textentryButton' );
 }
 
 function showMessage( startX, startY, content ) {
