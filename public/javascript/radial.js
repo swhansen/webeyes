274dd4405c219debec99aaf8a1cb1fd0b0ac535f
Steve Hansen <stevehansen@Steves-MacBook-Air-2.local>
2016-12-22 12:59:36 -0500

    svg ui graphics

--- public/javascript/radial.js
+++ public/javascript/radial.js
@@ -1,5 +1,5 @@
 function radialMenu( menu ) {
-if ( menu === 'ar ') {
+if ( menu === 'ar') {
 var svg = document.getElementById( 'radial-menu' ),
     items = svg.querySelectorAll( '.item' ),
     trigger = svg.getElementById( 'trigger' ),
