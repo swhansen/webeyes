0c53e7284e8d8183c59f87c8517287eb1f51ea16
Steve Hansen <stevehansen@Steves-MacBook-Air-2.local>
2016-12-29 18:03:59 -0500

    api development

--- public/models/users.js
+++ public/models/users.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 var userSchema = new Schema( {
   firstName:String,
   lastName:String,
-  email: String
+  email: String,
+  org: String
 } );
 
 module.exports = mongoose.model( 'users', userSchema );
