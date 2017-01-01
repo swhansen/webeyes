8a18bbe8207a9557ce6722b80b76071b11eb6eb6
Steve Hansen <stevehansen@Steves-MacBook-Air-2.local>
2017-01-01 14:44:57 -0500

    api development

--- public/javascript/setup-ar-interaction.js
+++ public/javascript/setup-ar-interaction.js
@@ -5,7 +5,7 @@ function setupArInteractionEvents() {
 //  - set the cameraDriver based on AR/VR and focus/peer
 //
 
-socketServer.on( 'placeArObject', function( data ) ) {
+socketServer.on( 'placeArObject', function()  {
   console.log( 'recieved placeArObject');
   } );
 
