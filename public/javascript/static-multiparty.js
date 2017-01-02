15c5897dbc5dca9a1ca2943ec76bf843ff7ce92e
Steve Hansen <stevehansen@Steves-MacBook-Air-2.local>
2017-01-02 13:37:32 -0500

    api development

--- public/javascript/static-multiparty.js
+++ public/javascript/static-multiparty.js
@@ -79,7 +79,7 @@ socketServer.on( 'roomnamerequest', function( data ) {
 } );
 
 socketServer.on( 'placeArObject', function( data ) {
-  console.log( 'Test Msg at static-multiparty recieved on placeArObject' );
+  console.log( 'Test Msg at static-multiparty recieved on placeArObject', data );
   } );
 
   var sessionId = socketServer.sessionid;
