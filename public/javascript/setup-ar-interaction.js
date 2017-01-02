5ebe5b2e70cccec7aa9581cc55f059dedd68d92d
Steve Hansen <stevehansen@Steves-MacBook-Air-2.local>
2017-01-02 13:11:35 -0500

    api development

--- public/javascript/setup-ar-interaction.js
+++ public/javascript/setup-ar-interaction.js
@@ -6,7 +6,7 @@ function setupArInteractionEvents() {
 //
 
 socketServer.on( 'placeArObject', function( data ) {
-  console.log( 'recieved placeArObject', data.event, data.object );
+  console.log( 'recieved placeArObject' data );
   } );
 
 var arCanvas = document.getElementById( 'arcanvaspane' );
