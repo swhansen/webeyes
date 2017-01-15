3ac31919117b4aec5c3eb9ae14fb07c9c7eb81a1
Steve Hansen <stevehansen@Steves-Air-2.bad>
2017-01-15 17:06:46 -0500

    radial menu dev

--- public/javascript/radial.js
+++ public/javascript/radial.js
@@ -29,8 +29,8 @@ var arRadialSvg = document.getElementById( 'ar-radial-menu' ),
   drawRadialSvg.style.pointerEvents = 'none';
 
 
- drawRadialSvg.style.visibility = 'hidden';
-drawRadialTrigger.style.visibility = 'hidden';
+// drawRadialSvg.style.visibility = 'hidden';
+// drawRadialTrigger.style.visibility = 'hidden';
 
 
 function moveRadialtoTop( radialMenu ) {
