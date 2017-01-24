d90a76ea5be70ee3463822fe1db2a5f84aaf848c
Steve Hansen <sw_hansen@obliquevision.org>
2017-01-23 22:32:20 -0500

    radial menu dev

--- public/javascript/radial.js
+++ public/javascript/radial.js
@@ -90,7 +90,7 @@ $( '#draw-radial-item-5' ).css( 'visibility', 'hidden' );
 
  // $( '#draw-radial-item-3 .sector' ).style.fill = '#DE5B32';
 
-  $('#draw-radial-item-3').children('path').find('.sector').style.fill = '#DE5B32';
+  //$('#draw-radial-item-3').children('path').find('.sector').style.fill = '#DE5B32';
 
 
     highlightSelectedRadial( 'draw-radial-circle' );
