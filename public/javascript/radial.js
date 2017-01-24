8a45dca5e31bb2ae84b4ff0910567da83064d6e8
Steve Hansen <sw_hansen@obliquevision.org>
2017-01-24 07:52:32 -0500

    radial menu dev

--- public/javascript/radial.js
+++ public/javascript/radial.js
@@ -121,7 +121,7 @@ $( '#draw-radial-item-5' ).css( 'visibility', 'hidden' );
 
 
       console.log( 'drawradialSVG:', e.target );
-     console.log( 'target:', $( e.target ).text );
+     console.log( 'target:', $( e.target.id );
 
 
    //   $('#draw-radial-item-3').children('path').css({ fill: "#ff0000" });
