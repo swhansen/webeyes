997c62dfcfe56bbf7f2b7be714b9b1b77a1e31cc
Steve Hansen <sw_hansen@obliquevision.org>
2017-01-25 19:10:54 -0500

    swal2 upgrade

--- public/javascript/plusup-code.js
+++ public/javascript/plusup-code.js
@@ -300,53 +300,53 @@ $( '#codeDialogModal' ).dialog( {
         autoOpen: false
     } );
 
-$( '#codeDialogButton' ).click( function() {
- swal( {
-   title: 'Plus-up Code',
-   text: 'Input your Super Secret Code',
-   input: 'text',
-   showCancelButton: true,
-   closeOnCancel: true,
-   closeOnConfirm: false,
-   animation: 'slide-from-top',
-   inputPlaceholder: 'Plus-up Code',
-     } ).then(
-       function(inputValue){
-         if ( inputValue === false ) return false;
-         if ( inputValue.toLowerCase() === 'augme' && userContext.arCapable === false) {
-           swal('Oops... Not an AR Capable Device', 'Use a orientation and location aware device', 'error' );
-         } else {
-           if (inputValue === '') {
-             swal.showInputError('Please Enter Code!');
-             return false;
-           }
-          else
-           if ( !( _.includes( [ 'devme', 'modme', 'augme', 'vrme', 'iots', 'iotc', 'leapme', 'iotz', 'clearmodme', 'leapgrabon', 'leapgraboff' ],
-             inputValue.toLowerCase() ) ) ) {
-               swal.showInputError( 'Please enter a valid code' );
-             return false;
-         }
-            if ( _.includes( [ 'devme', 'modme', 'augme', 'vrme', 'iots', 'iotc', 'leapme', 'iotz', 'clearmodme', 'leapgrabon', 'leapgraboff' ],
-             inputValue.toLowerCase() ) ) {
-               if ( inputValue.toLowerCase() === 'iotz' ) {
-                 useModeCode( inputValue.toLowerCase() );
-               }  else
-               if ( inputValue.toLowerCase() === 'vrme' ) {
-                 useModeCode( inputValue.toLowerCase() );
-               }
-               else
-               if ( inputValue.toLowerCase() === 'augme' ) {
-                 useModeCode( inputValue.toLowerCase() );
-               }
-             else {
-               swal.close();
-               useModeCode( inputValue.toLowerCase() );
-               }
-             }
-           }
-     }
-     );
- } );
+//$( '#codeDialogButton' ).click( function() {
+// swal( {
+//   title: 'Plus-up Code',
+//   text: 'Input your Super Secret Code',
+//   input: 'text',
+//   showCancelButton: true,
+//   closeOnCancel: true,
+//   closeOnConfirm: false,
+//   animation: 'slide-from-top',
+//   inputPlaceholder: 'Plus-up Code',
+//     } ).then(
+//       function(inputValue){
+//         if ( inputValue === false ) return false;
+//         if ( inputValue.toLowerCase() === 'augme' && userContext.arCapable === false) {
+//           swal('Oops... Not an AR Capable Device', 'Use a orientation and location aware device', 'error' );
+//         } else {
+//           if (inputValue === '') {
+//             swal.showInputError('Please Enter Code!');
+//             return false;
+//           }
+//          else
+//           if ( !( _.includes( [ 'devme', 'modme', 'augme', 'vrme', 'iots', 'iotc', 'leapme', 'iotz', 'clearmodme', 'leapgrabon', 'leapgraboff' ],
+//             inputValue.toLowerCase() ) ) ) {
+//               swal.showInputError( 'Please enter a valid code' );
+//             return false;
+//         }
+//            if ( _.includes( [ 'devme', 'modme', 'augme', 'vrme', 'iots', 'iotc', 'leapme', 'iotz', 'clearmodme', 'leapgrabon', 'leapgraboff' ],
+//             inputValue.toLowerCase() ) ) {
+//               if ( inputValue.toLowerCase() === 'iotz' ) {
+//                 useModeCode( inputValue.toLowerCase() );
+//               }  else
+//               if ( inputValue.toLowerCase() === 'vrme' ) {
+//                 useModeCode( inputValue.toLowerCase() );
+//               }
+//               else
+//               if ( inputValue.toLowerCase() === 'augme' ) {
+//                 useModeCode( inputValue.toLowerCase() );
+//               }
+//             else {
+//               swal.close();
+//               useModeCode( inputValue.toLowerCase() );
+//               }
+//             }
+//           }
+//     }
+//     );
+// } );
 
 
 $( '#codeDialogButton' ).click( function() {
