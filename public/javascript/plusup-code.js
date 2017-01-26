cb11a9d523a31b7b8b580c71b0674c6e5758cd39
Steve Hansen <sw_hansen@obliquevision.org>
2017-01-25 22:13:40 -0500

    swal2 upgrade

--- public/javascript/plusup-code.js
+++ public/javascript/plusup-code.js
@@ -319,13 +319,18 @@ function vrWorldModal() {
 function helpModal() {
   console.log( 'at helpModal' );
 swal({
-  title: 'jQuery HTML example',
-  html: $('<div>')
-    .addClass('some-class')
-    .text('jQuery is everywhere.'),
-  animation: false,
-  customClass: 'animated tada'
-})
+  title: 'Auto close alert!',
+  text: 'I will close in 2 seconds.',
+  timer: 2000
+}).then(
+  function () {},
+  // handling the promise rejection
+  function (dismiss) {
+    if (dismiss === 'timer') {
+      console.log('I was closed by the timer')
+    }
+  }
+)
 }
 
 
