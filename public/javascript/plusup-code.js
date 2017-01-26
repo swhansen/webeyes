57494ece4fa3315613a6a261eb8d987f842d1815
Steve Hansen <sw_hansen@obliquevision.org>
2017-01-25 22:15:03 -0500

    swal2 upgrade

--- public/javascript/plusup-code.js
+++ public/javascript/plusup-code.js
@@ -197,7 +197,19 @@ function useModeCode( modeCode ) {
 
   case 'help':
   console.log('at case helpModal' );
-    helpModal();
+    swal({
+  title: 'Auto close alert!',
+  text: 'I will close in 2 seconds.',
+  timer: 2000
+}).then(
+  function () {},
+  // handling the promise rejection
+  function (dismiss) {
+    if (dismiss === 'timer') {
+      console.log('I was closed by the timer')
+    }
+  }
+);
   break;
   }
 }
