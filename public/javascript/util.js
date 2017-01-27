4742d0351e53bd8dd62b69950a98e79a6565af96
Steve Hansen <sw_hansen@obliquevision.org>
2017-01-27 11:50:23 -0500

    swal2 upgrade

--- public/javascript/util.js
+++ public/javascript/util.js
@@ -78,20 +78,18 @@ function openFileModal() {
 
 
 socketServer.on( 'shareImage', function( data ) {
-  image( 'fred', data.source)
- //try{
- //  img = new Image();
- //  img.width = data.width;
- //  img.height = data.height;
- //  img.src = data.source;
-
- //  ctx.drawImage( img, 0, 0, box0Width, box0Height );
- //  console.log( 'serveron-sharechange:', img.width, img.height, img.src );
-
- //}
- //  catch( err ) {
- //    console.log( 'shareImage-on:', err)
- //}
+ // image( 'fred', data.source)
+ try{
+//   img = new Image();
+//   img.width = data.width;
+//   img.height = data.height;
+//   img.src = data.source;
+  ctx.drawImage( img.src, 0, 0, box0Width, box0Height );
+   console.log( 'serveron-sharechange:', img.width, img.height, img.src );
+}
+   catch( err ) {
+     console.log( 'shareImage-on:', err)
+ }
 } );
 
 function image (from, base64Image) {
