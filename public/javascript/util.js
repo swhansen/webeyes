c24338a21a47edcfd3b2abe7e62f6642a6325868
Steve Hansen <sw_hansen@obliquevision.org>
2017-01-30 09:39:05 -0500

    util canvas sizing

--- public/javascript/util.js
+++ public/javascript/util.js
@@ -8,42 +8,23 @@ var d2 = document.getElementById('doc-2');
 var d3 = document.getElementById('material');
 
 
-var c = document.getElementById( "myCanvas" );
+var c = document.getElementById("myCanvas");
+var myctx = c.getContext("2d");
+myctx.moveTo(0,0);
+myctx.lineTo(200,100);
+myctx.stroke();
 
-var myctx = c.getContext( "2d" );
 
-function drawMyCanvas() {
-var outerRadius = 300;
-  var innerRadius = 275;
-  myctx.beginPath();
-  myctx.arc(500, 400, outerRadius, 0, 2 * Math.PI, false);
-  myctx.fillStyle = 'rgba(255,255,255,.2)';
-  myctx.fill();
-  myctx.globalCompositeOperation = 'destination-out';
-  myctx.beginPath();
-  myctx.arc(500, 400, innerRadius, 0, 2 * Math.PI, false);
-  myctx.fillStyle = 'green';
-  myctx.fill();
-  myctx.globalCompositeOperation = 'source-over';
-  myctx.beginPath();
-  myctx.moveTo(500 - innerRadius, 400);
-  myctx.lineTo(500 + innerRadius, 400);
-  myctx.stroke();
-  myctx.moveTo(500, 400 - innerRadius);
-  myctx.lineTo(500, 400 + innerRadius);
-  myctx.stroke();
-  console.log( 'drawBullsEye - end:' );
-}
 
-drawMyCanvas();
+
+
+
+
+
 
 
 
 function initUtil() {
-
-
-
-
 
   userContext.addDimensionalLayer( 'utilcanvaspane' );
 
