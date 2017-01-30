616a8234a3d1352af9e8adc34d3deb9074cfba6d
Steve Hansen <sw_hansen@obliquevision.org>
2017-01-30 11:03:47 -0500

    util canvas sizing

--- public/javascript/util.js
+++ public/javascript/util.js
@@ -58,6 +58,10 @@ var boxHeight = box.outerHeight();
 
 
   function myDraw() {
+
+var MyDrawDiv = $( '#mydrawdiv');
+myDrawDiv.css(  'width', 600 );
+myDrawDiv.css(  'height', 600 );
 var c = document.getElementById("myCanvas");
 myCanvas.width  = 400;
 myCanvas.height = 300;
