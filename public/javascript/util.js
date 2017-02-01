11abf9414b349ec4d04b4fe7849658248436c99a
Steve Hansen <sw_hansen@obliquevision.org>
2017-02-01 09:43:29 -0500

    util canvas sizing

--- public/javascript/util.js
+++ public/javascript/util.js
@@ -100,7 +100,7 @@ socketServer.on( 'shareImage', function( data ) {
     if ( !imgRecieve ) { console.log( 'no data over the pipe' ); }
     docctx.drawImage( imgRecieve, 0, 0, box0Width, box0Height );
 
-    $("#myimg").attr("src", data.source );
+  //  $("#myimg").attr("src", data.source );
 
     }
   } );
